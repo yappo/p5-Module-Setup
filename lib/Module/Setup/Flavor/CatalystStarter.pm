59e6db79771b275d5aecd06b1a83044afc307b05
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2008-10-24 09:10:28 +0000

    use Module::Setup::Flavor::SelectVC

--- lib/Module/Setup/Flavor/CatalystStarter.pm
+++ lib/Module/Setup/Flavor/CatalystStarter.pm
@@ -1,39 +1,7 @@
 package Module::Setup::Flavor::CatalystStarter;
 use strict;
 use warnings;
-use base 'Module::Setup::Flavor';
-
-sub setup_config {
-    my($class, $context, $config) = @_;
-
-    my %vcs = (
-        SVN => 'VC::SVN',
-        SVK => 'VC::SVK',
-        Git => 'VC::Git',
-    );
-
-    for my $name (qw/ SVN SVK Git /) {
-        my $pkg = $vcs{$name};
-        next unless $context->dialog("Do you use $name? [yN]", 'n') =~ /[Yy]/;
-        $context->log("You chose version control system: $name");
-
-        next if grep {
-            ( ref($_) eq 'HASH' && $_->{module} eq $pkg) || $_ eq $pkg
-        } @{ $config->{plugins} };
-
-        push @{ $config->{plugins} }, $pkg;
-    }
-
-    if(grep {
-        ( ref($_) eq 'HASH' && $_->{module} eq 'VC::SVK') || $_ eq 'VC::SVK'
-    } @{ $config->{plugins} }) {
-        my @plugins = grep {
-            !(( ref($_) eq 'HASH' && $_->{module} eq 'VC::SVN') || $_ eq 'VC::SVN')
-        } @{ $config->{plugins} };
-        $config->{plugins} = \@plugins;
-    }
-
-}
+use base 'Module::Setup::Flavor::SelectVC';
 
 1;
 
